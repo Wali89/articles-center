42812054b8bdb95bdde7942274057e81a36bf7db
wali89 <wali.rahman89@yahoo.com>
2019-02-19 20:29:14 -0500

    Cleaned code

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -13,16 +13,24 @@ const Navbar = () => {
           <div className="ui-tabular-stackable-menu">
             <button className="button"><Link to="/" id="TopStories-link" className="item"> Top Stories </Link></button>
             <button className="button"><Link to="/channels"> Browse Channels </Link></button>
+
             <button className="button"><Link to="/search">Search Articles</Link></button>
-            <h1 id="title" className="title">Culture</h1>
+
+            <h1 id="title" className="title">Articles Center</h1>
+
           </div>
         </div>
+
         <div className="ui hidden divider"></div>
         {/*Routes */} 
         <Route exact path="/" component={TopStoriesContainer} />
+
         <Route exact path="/channels" component={ChannelsContainer} />
+
         <Route exact path="/channels/:id" component={ChannelArticlesContainer} />
+
         <Route exact path="/search" component={QueryContainer} />
+
         <Route exact path="/search/:query" component={QueryContainer} />
 
     </div>
