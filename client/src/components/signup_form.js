160fd48b8dce542b0028839abbff09132a1892da
wali89 <wali.rahman89@yahoo.com>
2019-04-07 17:23:03 -0400

    added signup form

---
+++ client/src/components/signup_form.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+import StatusMessage from 'containers/status_message';
+import {UserForm, UserInput, UserButton, UserLink, SubText} from 'components/user_form_style.js';
+
+class SignUpForm extends Component {
+
+    state = {
+        email: '',
+        password: ''
+    }
+
+    handleSignUp = (event) => {
+        event.preventDefault();
+        this.props.handleSubmit(this.state);
+    };
+
+    handleChange = (event) => this.setState({
+        [event.target.name]: event.target.value
+    });
+
+    render() {
+        return (
+            <UserForm>
+                <h1>news4u sign up</h1>
+                <StatusMessage />                
+                <form onSubmit={ event => this.handleSignUp(event) }>
+                    <UserInput type="text" name="email" onChange={ event => this.handleChange(event) } placeholder="User Email" />
+                    <UserInput type="password" name="password" onChange={ event => this.handleChange(event) } placeholder="Password" />
+                    <UserButton type="submit" >Sign Up</UserButton>
+                </form> 
+                <SubText>Already registered? <UserLink onClick={this.props.handleSwitchForm}>Login!</UserLink></SubText>              
+            </UserForm>        
+        );
+    }
+}
+
+export default SignUpForm;
