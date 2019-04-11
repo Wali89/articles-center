b9dd781d3bbca2dd5efa6a12b19d8dc0fab66e68
wali89 <wali.rahman89@yahoo.com>
2019-04-10 21:15:36 -0400

    added loginUser action

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -12,3 +12,32 @@ export function setUserMessage(msg) {
       payload: msg
   };
 }
+
+export function loginUser(credentials) {
+  return (dispatch) => {
+      dispatch(setUserMessage("Logging in..."));
+
+      const request = {"auth": {"email": credentials.email, "password": credentials.password}}
+      const options = {
+          method: 'POST',
+          body: JSON.stringify(request),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+      };
+      //console.log("Login User!",request);
+      fetch("api/user_token", options)
+          .then(res => handleAPIErrors(res))        
+          .then(res => res.json())
+          .then(res => {
+              localStorage.setItem("jwt", res.jwt); // TODO: move to reducer?
+              let id = jwt_decode(res.jwt).sub;                
+              dispatch({type:"LOGIN_USER", token:res.jwt, id: id})
+              dispatch(getUserPreferences(id))               
+          })
+          .catch(function(error) {
+              console.log("Login Error",error);
+              dispatch(setUserMessage(`Login ${error}`));
+          });             
+  };
+}
