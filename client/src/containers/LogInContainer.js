a1214b3ca4968e52b092d833258fd4562e902f09
wali89 <wali.rahman89@yahoo.com>
2019-04-13 18:13:26 -0400

    Added log in container

---
+++ client/src/containers/LogInContainer.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import SignUpForm from 'components/signup_form';
+import LoginForm from 'components/login_form';
+import { loginUser, signUpUser } from 'actions/user';
+
+
+class LoginContainer extends Component {
+
+  state = {
+    newUser: false
+  }
+
+  handleSwitchForm =  () => {
+    this.setState((prevState) => ({newUser: !prevState.newUser}));
+  }
+
+  render() {
+    let form = this.state.newUser ?
+    <SignUpForm handleSwitchForm={this.handleSwitchForm} handleSubmit={this.props.signUpUser}/>
+    : <LoginForm handleSwitchForm={this.handleSwitchForm} handleSubmit={this.props.loginUser}/>
+  }
+
+  return (
+    {form}
+  )
+}
