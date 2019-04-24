7d01fdc61ebbc6a24232d5ae0089e137a48d5943
wali89 <wali.rahman89@yahoo.com>
2019-04-23 20:31:28 -0400

    added status message container

--- client/src/containers/status_message.js
+++ client/src/containers/status_message.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 
-export const UserMessage = style.div`
+export const UserMessage = styled.div` 
   color:red;
   font-size: 1.5em;
 `;
