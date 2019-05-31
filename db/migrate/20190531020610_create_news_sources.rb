6ece0dddc04139174f713a0aa1d55a26af28520d
wali89 <wali.rahman89@yahoo.com>
2019-05-30 22:11:04 -0400

    added new source migration & model

--- db/migrate/20190531020610_create_news_sources.rb
+++ db/migrate/20190531020610_create_news_sources.rb
@@ -1,13 +1,12 @@
 class CreateNewsSources < ActiveRecord::Migration[5.2]
   def change
     create_table :news_sources do |t|
-      t.news_source_id :integer
-      t.name :string
-      t.description :text
-      t.url :string
-      t.category :string
-      t.country :string
-
+      t.string :news_source_id
+      t.string :name
+      t.text :description
+      t.string :url
+      t.string :category
+      t.string :country
       t.timestamps
     end
   end
