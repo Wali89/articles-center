714636b5beb4771004688fefb89c0ebdd3b6e8ab
Wali <wali.rahman89@yahoo.com>
2019-09-28 18:25:28 -0400

    added to user reducer

--- client/src/reducers/user_reducer.js
+++ client/src/reducers/user_reducer.js
@@ -8,14 +8,24 @@ export default function userReducer(
 
   {
     switch(action.type) {
-      case "LOGIN_USER":
 
-      return {...state, statusMessage:"", jwt: action.token, id: action.id}
+      case "LOGIN_USER":
+        return {...state, statusMessage:"", jwt: action.token, id: action.id}
 
       case "SET_USER_PREFERENCES":
+        return {...state, isLoggedIn:true, newsSources:action.payload.news_sources}
 
-      return {...state, isLoggedIn:true, newsSources:action.payload.news_sources}
+      case "UPDATE_USER_PREFERENCES":
+        return {...state, newSources: action.payload.news_sources}
 
+      case "LOGGING_OUT":
+        return {...state, jwt: null, id: 0, isLoggedIn: false}
+
+      case "SET_USER_MESSAGE":
+        return {...state, statusMessage: action.payload}
+
+      default:
+        return state;
       
     }
   }
