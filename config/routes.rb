bdd18b16d488f96c9009056e82a7e09a2e24a008
Wali <wali.rahman89@yahoo.com>
2019-10-26 20:52:32 -0400

    updated rates

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,12 @@ Rails.application.routes.draw do
 
   scope :api do
 
-    resources :articles
+    
     get '/articles', to: 'articles#index'
     get '/topStories', to: 'articles#topStories'
     get '/channels', to: 'articles#channels'
     get '/channels/:channel', to: 'articles#channel_articles'
     post'/articles_search', to: 'articles#articles_search'
-    get '/searches', to: 'search#last_ten'
-    post '/searches/:id', to: 'search#update'
     get '/users/:id' => 'users#show'
     patch '/users/:id' => 'users#update'
     post '/users' => 'users#create'
