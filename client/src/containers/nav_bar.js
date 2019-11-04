c93c638825cd1fce5fe67fcbdd65b03f21e05ef4
Wali <wali.rahman89@yahoo.com>
2019-11-03 20:00:04 -0500

    changed to nav bar

--- client/src/containers/nav_bar.js
+++ client/src/containers/nav_bar.js
@@ -5,26 +5,64 @@ import ChannelsContainer from './channels';
 import ChannelArticlesContainer from './channel_articles';
 import QueryContainer from './query';
 import LogInContainer from './log_in';
+import { NavLink } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap'
+
+
+const NewsNavbar = styled(Navbar)`
+  width: 100%;
+  border: solid 5px #3F5941;
+  background-color: white;
+  color: #3F5941;
+  text-align: left;
+  position: fixed;
+  top: 0; 
+`;
+
+const NewsBrand = styled(NavLink)`
+    padding: 5px 12px 12px 12px;
+    margin: 0 0 0 0.5em;
+    color: #3F5941;
+    font-size: 48px;
+    font-weight: bold;
+    font-family: Impact, Times New Roman, Times, serif;
+`;
+
+const NewsLink = styled(NavLink)`
+    padding: 12px;
+    margin: 0 12px 0px 0px;
+    textDecoration: none;
+    color: black;
+    font-size: 18px;
+`;
 
 
 class NavBar extends Component {
   render() {
+    let userOptions = null;
+    let logoutOption=null;
+
+    if (this.props.userLoggedIn) {
+      userOptions= 
+      <React.Fragment>
+        <button className="button"><NewsLink to="/preferences"> Preferences </NewsLink></button>
+        <button className="button"><NewsLink to="/" id="TopStories-link" className="item"> Top Stories </NewsLink></button>
+
+        <button className="button"><NewsLink to="/channels"> Browse Channels </NewsLink></button>
+
+        <button className="button"><NewsLink to="/search">Search Articles</NewsLink></button>
+
+        <button className="button"><NewsLink to="/users">Log In</NewsLink></button> 
+
+        <h1 id="title" className="title">Articles Center</h1>
+      </React.Fragment>
+    };
   return(
     
     <Router>
-      <div>
+      
         <div className="App-navbar">
-          <div className="ui-tabular-stackable-menu">
-            <button className="button"><Link to="/" id="TopStories-link" className="item"> Top Stories </Link></button>
-            <button className="button"><Link to="/channels"> Browse Channels </Link></button>
-
-            <button className="button"><Link to="/search">Search Articles</Link></button>
-
-           <button className="button"><Link to="/users">Log In</Link></button> 
-
-            <h1 id="title" className="title">Articles Center</h1>
-
-          </div>
+        {userOptions}
         </div>
 
         <div className="ui hidden divider"></div>
@@ -41,11 +79,12 @@ class NavBar extends Component {
 
         <Route exact path="/users" component={LogInContainer} />
 
-    </div>
+        <Route exact path="/users" component={LogInContainer} />
 
+    
     </Router>
     
-  )
+    )
   }
 }
 
